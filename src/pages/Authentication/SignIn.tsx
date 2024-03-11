f08eb09a55439f377030743019985647cf2e50cc
Farshad Nabizadeh <farshadnabizadeh1993@gmail.com>
2024-03-11 13:35:31 -0700

    Login

--- src/pages/Authentication/SignIn.tsx
+++ src/pages/Authentication/SignIn.tsx
@@ -6,7 +6,7 @@ const SignIn: React.FC = () => {
   return (
     <React.Fragment>
       <main className='w-full min-h-screen relative'>
-        <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+        <div className="rounded-sm bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
           <div className="flex flex-wrap items-center w-full h-screen">
             <div className="hidden w-full xl:block xl:w-1/2">
               <div className="py-17.5 px-26 text-center">
@@ -186,12 +186,12 @@ const SignIn: React.FC = () => {
 
                   <div className="mb-6">
                     <label className="mb-2.5 block font-medium text-black dark:text-white">
-                      Re-type Password
+                      Password
                     </label>
                     <div className="relative">
                       <input
                         type="password"
-                        placeholder="6+ Characters, 1 Capital letter"
+                        placeholder="Enter Your Password"
                         className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                       />
 
@@ -263,15 +263,6 @@ const SignIn: React.FC = () => {
                     </span>
                     Sign in with Google
                   </button>
-
-                  <div className="mt-6 text-center">
-                    <p>
-                      Don’t have any account?{' '}
-                      <Link to="/auth/signup" className="text-primary">
-                        Sign Up
-                      </Link>
-                    </p>
-                  </div>
                 </form>
               </div>
             </div>
