afaab567ccbf5a9cff0a46b6b84225fd9ed71f60
Farshad Nabizadeh <farshadnabizadeh1993@gmail.com>
2024-03-13 14:47:53 -0700

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,17 @@ import { Route, Routes, useLocation } from 'react-router-dom';
 import Loader from './common/Loader';
 import PageTitle from './components/PageTitle';
 import SignIn from './pages/Authentication/SignIn';
-import SignUp from './pages/Authentication/SignUp';
-import Calendar from './pages/Calendar';
-import Chart from './pages/Chart';
-import ECommerce from './pages/Dashboard/ECommerce';
-import FormElements from './pages/Form/FormElements';
-import FormLayout from './pages/Form/FormLayout';
-import Profile from './pages/Profile';
-import Settings from './pages/Settings';
-import Tables from './pages/Tables';
-import Alerts from './pages/UiElements/Alerts';
-import Buttons from './pages/UiElements/Buttons';
+// import SignUp from './pages/Authentication/SignUp';
+// import Calendar from './pages/Calendar';
+// import Chart from './pages/Chart';
+// import ECommerce from './pages/Dashboard/ECommerce';
+// import FormElements from './pages/Form/FormElements';
+// import FormLayout from './pages/Form/FormLayout';
+// import Profile from './pages/Profile';
+// import Settings from './pages/Settings';
+// import Tables from './pages/Tables';
+// import Alerts from './pages/UiElements/Alerts';
+// import Buttons from './pages/UiElements/Buttons';
 
 function App() {
   const [loading, setLoading] = useState<boolean>(true);
