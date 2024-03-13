e5a3dafcdcd04c0bc4a72a5614247944c3d10a75
Farshad Nabizadeh <farshadnabizadeh1993@gmail.com>
2024-03-13 14:52:51 -0700

    updated

--- src/Memo.ts
+++ src/Memo.ts
@@ -1,29 +1,28 @@
-
-    import { createSlice } from "@reduxjs/toolkit";
-    const Menu: any = (state: any, action: any) => {
-        return {
-            state,
-            status: action,
-        };
+import { createSlice } from "@reduxjs/toolkit";
+const Menu: any = (state: any, action: any) => {
+    return {
+        state,
+        status: action,
     };
-    const Id: any = (state: any, action: any) => {
-        return {
-            state,
-            id: action,
-        };
+};
+const Id: any = (state: any, action: any) => {
+    return {
+        state,
+        id: action,
     };
-    const Memo = createSlice({
-        name: "Memo",
-        initialState: {
-            status: false,
-            id: 0,
-        },
-        reducers: {
-            Menu,
-            Id,
-        },
-    });
-    export const { Menu: MenuAction, Id: IdAction } = Memo.actions;
-    export default Memo.reducer;
+};
+const Memo = createSlice({
+    name: "Memo",
+    initialState: {
+        status: false,
+        id: 0,
+    },
+    reducers: {
+        Menu,
+        Id,
+    },
+});
+export const { Menu: MenuAction, Id: IdAction } = Memo.actions;
+export default Memo.reducer;
 
 
