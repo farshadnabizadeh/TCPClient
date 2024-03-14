d7f14df2f0a935b11f1af518683597acb6070aaf
Farshad Nabizadeh <farshadnabizadeh1993@gmail.com>
2024-03-14 11:12:24 -0700

    User Auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
 import { useEffect, useState } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
-
+import { useNavigate } from "react-router-dom";
 import Loader from './common/Loader';
 import PageTitle from './components/PageTitle';
 import SignIn from './pages/Authentication/SignIn';
 import { useSelector } from 'react-redux';
-// import SignUp from './pages/Authentication/SignUp';
-// import Calendar from './pages/Calendar';
-// import Chart from './pages/Chart';
-// import ECommerce from './pages/Dashboard/ECommerce';
-// import FormElements from './pages/Form/FormElements';
-// import FormLayout from './pages/Form/FormLayout';
-// import Profile from './pages/Profile';
-// import Settings from './pages/Settings';
-// import Tables from './pages/Tables';
-// import Alerts from './pages/UiElements/Alerts';
-// import Buttons from './pages/UiElements/Buttons';
+import SignUp from './pages/Authentication/SignUp';
+import Calendar from './pages/Calendar';
+import Chart from './pages/Chart';
+import ECommerce from './pages/Dashboard/ECommerce';
+import FormElements from './pages/Form/FormElements';
+import FormLayout from './pages/Form/FormLayout';
+import Profile from './pages/Profile';
+import Settings from './pages/Settings';
+import Tables from './pages/Tables';
+import Alerts from './pages/UiElements/Alerts';
+import Buttons from './pages/UiElements/Buttons';
 
 function App() {
   const [loading, setLoading] = useState<boolean>(true);
   const { pathname } = useLocation();
-  const router = useSelector((state: any)=>state.Memo);
-  console.log(router)
+  const router = useSelector((state: any) => state.Memo);
+  console.log(router?.id?.response)
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
@@ -36,17 +36,17 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<> <PageTitle title="Signin | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <SignIn /> </>} />
-        {/* <Route path="/" element={<> <PageTitle title="eCommerce Dashboard | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <ECommerce /> </>} /> */}
-        {/* <Route path="/calendar" element={<> <PageTitle title="Calendar | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <Calendar /> </>} /> */}
-        {/* <Route path="/profile" element={<> <PageTitle title="Profile | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <Profile /> </>} /> */}
-        {/* <Route path="/forms/form-elements" element={<> <PageTitle title="Form Elements | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <FormElements /> </>} /> */}
-        {/* <Route path="/forms/form-layout" element={<> <PageTitle title="Form Layout | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <FormLayout /> </>} /> */}
-        {/* <Route path="/tables" element={<> <PageTitle title="Tables | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <Tables /> </>} /> */}
-        {/* <Route path="/settings" element={<> <PageTitle title="Settings | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <Settings /> </>} /> */}
-        {/* <Route path="/chart" element={<> <PageTitle title="Basic Chart | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <Chart /> </>} /> */}
-        {/* <Route path="/ui/alerts" element={<> <PageTitle title="Alerts | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <Alerts /> </>} /> */}
-        {/* <Route path="/ui/buttons" element={<> <PageTitle title="Buttons | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <Buttons /> </>} /> */}
-        {/* <Route path="/auth/signup" element={<> <PageTitle title="Signup | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <SignUp /> </>} /> */}
+        {router?.id?.response && <Route path="/dashboard" element={<> <PageTitle title="eCommerce Dashboard | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <ECommerce /> </>} />}
+        {router?.id?.response && <Route path="/calendar" element={<> <PageTitle title="Calendar | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <Calendar /> </>} />}
+        {router?.id?.response && <Route path="/profile" element={<> <PageTitle title="Profile | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <Profile /> </>} />}
+        {router?.id?.response && <Route path="/forms/form-elements" element={<> <PageTitle title="Form Elements | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <FormElements /> </>} />}
+        {router?.id?.response && <Route path="/forms/form-layout" element={<> <PageTitle title="Form Layout | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <FormLayout /> </>} />}
+        {router?.id?.response && <Route path="/tables" element={<> <PageTitle title="Tables | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <Tables /> </>} />}
+        {router?.id?.response && <Route path="/settings" element={<> <PageTitle title="Settings | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <Settings /> </>} />}
+        {router?.id?.response && <Route path="/chart" element={<> <PageTitle title="Basic Chart | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <Chart /> </>} />}
+        {router?.id?.response && <Route path="/ui/alerts" element={<> <PageTitle title="Alerts | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <Alerts /> </>} />}
+        {router?.id?.response && <Route path="/ui/buttons" element={<> <PageTitle title="Buttons | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <Buttons /> </>} />}
+        {router?.id?.response && <Route path="/auth/signup" element={<> <PageTitle title="Signup | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <SignUp /> </>} />}
       </Routes>
     </>
 
