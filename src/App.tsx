2b47a9508e05e3ee97e3b4068fb63604d92c51c9
Farshad Nabizadeh <farshadnabizadeh1993@gmail.com>
2024-03-14 14:33:03 -0700

    RoutesEditV1

--- src/App.tsx
+++ src/App.tsx
@@ -19,8 +19,15 @@ import Buttons from './pages/UiElements/Buttons';
 function App() {
   const [loading, setLoading] = useState<boolean>(true);
   const { pathname } = useLocation();
+  const [routes, setRoutes] = useState<any>(false)
   const router = useSelector((state: any) => state.Memo);
-  console.log(router?.id?.response)
+  useEffect(() => {
+    if (router?.id?.response == undefined) {
+      setRoutes(false)
+    } else {
+      setRoutes(true)
+    }
+  }, [router])
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
@@ -36,51 +43,51 @@ function App() {
       <Routes>
         <Route path="/" element={<> <PageTitle title="Signin | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <SignIn /> </>} />
         <Route path="/dashboard" element=
-          {router?.id?.response ?
+          {routes ?
             <><PageTitle title="eCommerce Dashboard | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <ECommerce /></> :
             <> <PageTitle title="Signin | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <SignIn /> </>} />
         <Route path="/calendar" element=
-          {router?.id?.response ?
+          {routes ?
             <> <PageTitle title="Calendar | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <Calendar /> </> :
             <> <PageTitle title="Signin | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <SignIn /> </>} />
         <Route path="/profile" element=
-          {router?.id?.response ?
+          {routes ?
             <> <PageTitle title="Profile | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <Profile /> </> :
             <> <PageTitle title="Signin | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <SignIn /> </>} />
         <Route path="/forms/form-elements" element=
-          {router?.id?.response ?
+          {routes ?
             <> <PageTitle title="Form Elements | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <FormElements /> </> :
             <> <PageTitle title="Signin | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <SignIn /> </>} />
         <Route path="/forms/form-layout" element=
-          {router?.id?.response ?
+          {routes ?
             <>
               <PageTitle title="Form Layout | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <FormLayout /> </> :
             <> <PageTitle title="Signin | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <SignIn /> </>} />
         <Route path="/tables" element=
-          {router?.id?.response ?
+          {routes ?
             <>
               <PageTitle title="Tables | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <Tables /> </> :
             <> <PageTitle title="Signin | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <SignIn /> </>
           } />
         <Route path="/settings" element=
-          {router?.id?.response ?
+          {routes ?
             <> <PageTitle title="Settings | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <Settings /> </> :
             <> <PageTitle title="Signin | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <SignIn /> </>
           } />
         <Route path="/chart" element=
-          {router?.id?.response ?
+          {routes ?
             <> <PageTitle title="Basic Chart | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <Chart /> </> :
             <> <PageTitle title="Signin | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <SignIn /> </>} />
         <Route path="/ui/alerts" element=
-          {router?.id?.response ?
+          {routes ?
             <> <PageTitle title="Alerts | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <Alerts /> </> :
             <> <PageTitle title="Signin | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <SignIn /> </>} />
         <Route path="/ui/buttons" element=
-          {router?.id?.response ?
+          {routes ?
             <> <PageTitle title="Buttons | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <Buttons /> </> :
             <> <PageTitle title="Signin | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <SignIn /> </>} />
         <Route path="/auth/signup" element=
-          {router?.id?.response ?
+          {routes ?
             <> <PageTitle title="Signup | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <SignUp /> </> :
             <> <PageTitle title="Signin | TailAdmin - Tailwind CSS Admin Dashboard Template" /> <SignIn /> </>} />
       </Routes>
