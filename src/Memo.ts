440c99ca43ad7b138aad35d6413ec2a43b96e590
Farshad Nabizadeh <farshadnabizadeh1993@gmail.com>
2024-03-18 11:23:33 -0700

    updated

--- src/Memo.ts
+++ src/Memo.ts
@@ -7,18 +7,22 @@ const Link = (state: any, action: any) => {
 const User = (state: any, action: any) => {
     state.userinfo = action.payload; // Assuming action has a payload property
 };
-
+const ZohoStorage = (state: any, action: any) => {
+    state.zoho = action.payload; // Assuming action has a payload property
+};
 const Memo = createSlice({
     name: "Memo",
     initialState: {
         id: '/',
         userinfo: null,
+        zoho: null
     },
     reducers: {
         Link,
         User,
+        ZohoStorage,
     },
 });
 
-export const { Link: LinkAction, User: UserAction } = Memo.actions;
+export const { Link: LinkAction, User: UserAction, ZohoStorage: ZohoStorageAction } = Memo.actions;
 export default Memo.reducer;
